a1168375e08f149228020b4665ee38a8c59d8374
Patrick Dai <patrickdai917@gmail.com>
2025-12-27 18:25:54 -0500

    Cleaned up repo. Added Shader Helper to compile and link shader code.

---
+++ Water/common/ShaderHelper.h
@@ -0,0 +1,183 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cstdlib>
+#include <cstdarg>
+#include <vector>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+
+class ShaderHelper
+{
+public:
+
+	// Shader constructor
+	ShaderHelper() {}
+
+	/* Reads and parses shader code and returns a compiled
+	version of the shader code. */
+	GLuint readShader(const char* filename, GLenum shaderType)
+	{
+		std::string shaderCode;
+
+		// Read shader code into string 
+		std::ifstream file_stream(filename, std::ios::in);
+		if (file_stream.is_open()) {
+			shaderCode = std::string(std::istreambuf_iterator<char>(file_stream),
+				std::istreambuf_iterator<char>());
+			file_stream.close();
+		}
+		else {
+			std::cout << "Could not open " << filename << std::endl;
+			return 0;
+		}
+
+		// Compile shader
+		GLuint shader = glCreateShader(shaderType);
+		const char* shaderCStrCode = shaderCode.c_str();
+		GLint shaderSourceLength = static_cast<GLuint>(strlen(shaderCStrCode));
+		glShaderSource(shader, 1, &shaderCStrCode, &shaderSourceLength);
+		glCompileShader(shader);
+
+		return shader;
+		
+	}
+
+	/* Compiles and links vertex and fragment shaders. Function returns
+	a linked Shader Program. */
+	GLuint compileShaders(const char* vShaderFilename, const char* fShaderFilename) {
+		// Compile both shaders
+		GLuint vShader = compileVShader(vShaderFilename);
+		GLuint fShader = compileFShader(fShaderFilename);
+
+		// Compile and link our shaders
+		GLuint shaderProgram = glCreateProgram();
+		glAttachShader(shaderProgram, vShader);
+		glAttachShader(shaderProgram, fShader);
+		glLinkProgram(shaderProgram);
+
+		// Check linking success
+		int  success;
+		char infoLog[512];
+		glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+		if (!success)
+		{
+			glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+			std::cout << "ERROR::SHADER::PROGRAM::LINK_FAILED\n" << infoLog << std::endl;
+			return 0;
+		}
+
+		glDeleteShader(vShader);
+		glDeleteShader(fShader);
+
+		return shaderProgram;
+	}
+
+	/* Returns a full linked shader program based on list
+	of ALREADY compiled shaders. */
+	GLuint linkShaders(int N...) {
+
+		va_list args;
+		va_start(args, N);
+
+		GLuint shaderProgram = glCreateProgram();
+
+		for (int i = 0; i < N; i++) {
+			GLuint shader = va_arg(args, GLuint);
+			glAttachShader(shaderProgram, shader);
+		}
+
+		glLinkProgram(shaderProgram);
+
+		// Check linking success
+		int  success;
+		char infoLog[512];
+		glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+		if (!success)
+		{
+			glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+			std::cout << "ERROR::SHADER::PROGRAM::LINK_FAILED\n" << infoLog << std::endl;
+			return 0;
+		}
+
+		// Don't forget to delete your shaders.
+
+		return shaderProgram;
+	}
+
+	// Returns a compiled vertex shader
+	GLuint compileVShader(const char* vShaderFilename) {
+		// Compile vertex shader
+		GLuint vShader = this->readShader(vShaderFilename, GL_VERTEX_SHADER);
+
+		// Check success of vShader compilation
+		int  success;
+		char infoLog[512];
+		glGetShaderiv(vShader, GL_COMPILE_STATUS, &success);
+		if (!success)
+		{
+			glGetShaderInfoLog(vShader, 512, NULL, infoLog);
+			std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+			glDeleteShader(vShader);
+			return 0;
+		}
+
+		return vShader;
+	}
+
+	// Returns a compiled fragment shader
+	GLuint compileFShader(const char* fShaderFilename) {
+		// Compile vertex shader
+		GLuint fShader = this->readShader(fShaderFilename, GL_FRAGMENT_SHADER);
+
+		// Check success of fShader compilation
+		int  success;
+		char infoLog[512];
+		glGetShaderiv(fShader, GL_COMPILE_STATUS, &success);
+		if (!success)
+		{
+			glGetShaderInfoLog(fShader, 512, NULL, infoLog);
+			std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+			glDeleteShader(fShader);
+			return 0;
+		}
+
+		return fShader;
+	}
+	
+	void setUniformMat4fv(GLuint shaderProgram, const char* uniformName, const GLfloat* uniformValue) {
+		glUseProgram(shaderProgram);
+		GLuint loc = glGetUniformLocation(shaderProgram, uniformName);
+		glUniformMatrix4fv(loc, 1, GL_FALSE, uniformValue);
+		glUseProgram(0);
+	}
+
+	void setUniform2fv(GLuint shaderProgram, const char* uniformName, const GLfloat* uniformValue) {
+		glUseProgram(shaderProgram);
+		GLuint loc = glGetUniformLocation(shaderProgram, uniformName);
+		glUniform2fv(loc, 1, uniformValue);
+		glUseProgram(0);
+	}
+
+	void setUniform3fv(GLuint shaderProgram, const char* uniformName, const GLfloat* uniformValue) {
+		glUseProgram(shaderProgram);
+		GLuint loc = glGetUniformLocation(shaderProgram, uniformName);
+		glUniform3fv(loc, 1, uniformValue);
+		glUseProgram(0);
+	}
+
+	void setUniform1f(GLuint shaderProgram, const char* uniformName, const GLfloat uniformValue) {
+		glUseProgram(shaderProgram);
+		GLuint loc = glGetUniformLocation(shaderProgram, uniformName);
+		glUniform1f(loc, uniformValue);
+		glUseProgram(0);
+	}
+
+	void useShaderProgram(GLuint shaderProgram) {
+		glUseProgram(shaderProgram);
+	}
+};
--- Water/main.cpp
+++ Water/main.cpp
@@ -1,8 +1,93 @@
 #include <iostream>
 
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+#include "common/ShaderHelper.h"
+
+const char* VERTEX_SHADER_PATH = "shaders/VertexShader.glsl";
+const char* FRAGMENT_SHADER_PATH = "shaders/FragmentShader.glsl";
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
 int main() {
 
-	std::cout << "HI" << std::endl;
+    // Initializing OpenGL windows.
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	return 0;
+    // Making the window
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    // Loading GLAD
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // Setting viewport size.
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glViewport(0, 0, 800, 600);
+
+    // Shader compilation and linking
+    ShaderHelper sh;
+    GLuint vShader = sh.compileVShader(VERTEX_SHADER_PATH);
+    GLuint fShader = sh.compileFShader(FRAGMENT_SHADER_PATH);
+    GLuint shaderProgram = sh.linkShaders(2, vShader, fShader);
+    glDeleteShader(vShader);
+    glDeleteShader(fShader);
+
+    // Render loop
+    while (!glfwWindowShouldClose(window))
+    {
+        // Processing keyboard inputs
+        processInput(window);
+
+        // Clearing screen on each render
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /*
+        Typically applications have two buffers. One is the front one that holds what is currently
+        being shown on the screen. The back buffer is the one we are updating. When the update is done, 
+        we simply swap the two.
+        */
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // Cleaning up the GLFW window.
+    glfwTerminate();
+
+    return 0;
 }
+
+/*
+For automatically resizing viewport if user changes the viewport size.
+*/
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+/*
+Processes keyboard inputs.
+*/
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
